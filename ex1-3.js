81204c3bd3de4e5de65860a18fbf1c73cff8bba0
oonoharuki <zm18244@josai.ac.jp>
2020-01-21 04:11:05 +0000

    1/21

--- ex1-3.js
+++ ex1-3.js
@@ -3,9 +3,9 @@ var who13 = '大野陽輝';
 function sayhello13(){
     alert('10月28日,'+who13+'!');
 }
-function 生年月日13(){
-    who13='住所';
-}
 function 住所13(){
-    who13
+    who13='群馬県高崎市';
+}
+function 特技13(){
+    who13='空手';
 }
