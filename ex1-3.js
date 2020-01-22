52f4cb7a9505419536b753c26ba60d3a11c4de44
oonoharuki <zm18244@josai.ac.jp>
2020-01-22 05:49:41 +0000

    1/22

--- ex1-3.js
+++ ex1-3.js
@@ -4,8 +4,8 @@ function sayhello13(){
     alert('10月28日,'+who13+'!');
 }
 function gunmakentakasakishi13(){
-    who13='群馬県高崎市,';
+    who13='群馬県高崎市';
 }
 function karate13(){
-    who13='空手,';
+    who13='空手';
 }
