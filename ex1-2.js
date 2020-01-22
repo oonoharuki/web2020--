83b87b1db0005ea6551f2ebcbb485821d129dfc0
oonoharuki <zm18244@josai.ac.jp>
2020-01-22 06:17:53 +0000

    1/22

--- ex1-2.js
+++ ex1-2.js
@@ -2,6 +2,8 @@ function textchange1(){
     document.getElementById('text1').innerHTML='群馬ちゃん!!';
 }
 
-
+function imgchange1(){
+    document.getElementById('logo').src = './image/ダウンロード.png';
+}
 
 
