97b64906e0dad2554dfd7f79c289aec620554931
oonoharuki <zm18244@josai.ac.jp>
2020-01-24 07:17:48 +0000

    1/24

--- ex1-4.js
+++ ex1-4.js
@@ -1,7 +1,7 @@
 
 function hyoji(){
 //表示する文字
-var str="一文字ずつ表示します。";
+var str="ご清聴ありがとうございました!!";
 //テキストボックスの文字数
 var cnt = document.timer.moji.value.length;
 //文字が全部表示されているか確認
@@ -17,5 +17,5 @@ function startfnc(){
 setInterval("hyoji()",1000);}
 
 function mes(){
-    alert("3秒経ちました!");
+    alert("ありがとうございました!");
 }
